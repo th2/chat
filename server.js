5d8f8bd1b2fb5d5a50c5e6164a30951f9ec02a64
Thorben Hehmann <th2@users.noreply.github.com>
2024-08-22 10:54:02 +0200

    add logging

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const logger = require('./logger');
+const config = require('./config.json');
 
-const port = process.env.PORT || 3001;
-
+app.use(logger.visit());
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+app.use(logger.error());
 
 io.on('connection', function(socket){
     socket.broadcast.emit('connected to server');
@@ -21,6 +23,6 @@ io.on('connection', function(socket){
     });
   });
 
-http.listen(port, function(){
-  console.log(`listening on ${port}`);
+http.listen(config.port, function(){
+  console.log(`listening on ${config.port}`);
 });
